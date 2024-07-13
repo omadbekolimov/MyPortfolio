94ec4f9f253dc1312ef72fff01f4d942025ff8fd
omadbekolimov <omadbekolimov96@gmail.com>
2024-07-13 15:12:30 +0500

    goo

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,18 +1,79 @@
 import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./Projects.css";
+const projects = [
+  {
+    id: 1,
+    title: "DELIVERY SERVICE",
+    imgSrc: "/peak-landing.png",
+    imgAlt: "peak performance landing page",
+    demoLink: "http://gsrlogistic.uz/",
+    githubLink: "https://github.com/Alisher0903/logistika",
+    description: `
+      The main purpose of this website is to deliver products from a foreign country to its users on time! In this service, the main goal of the website is to maintain the loyalty of its users, to achieve this result, users can see their product vertual and see when it will arrive and the price of the service, all the products are added from the admin panel. Estimated earnings of this website are between $1000-$10,000 depending on the sales of the products.
+      This program was completed and delivered to users directly within 10 days given 4 front end and 3 back end developers!
+    `,
+    technologies: {
+      backend: "Java",
+      frontend: "React Js",
+      database: "Postgres SQL",
+    },
+    tools: ["Java", "Tailwind CSS", "React Js", "PostgreSQL"],
+  },
+  {
+    id: 2,
+    title: "Railway ",
+    imgSrc: "/alibi-esports-landing.PNG",
+    imgAlt: "alibi esports landing page",
+    demoLink: "https://pch14.uz/",
+    githubLink: "https://github.com/SardorbekCoder07/Railway_z",
+    description: `
+      This web application is aimed at facilitating the work of all railway enterprises in Kashkadarya region. Through this web program, we brought a net profit of $10,000 to the company!
+    Currently, it is used by all railways, but only Kashkadarya currently uses it.
+    Date: 06/13/2024`,
+    technologies: {
+      backend: "Spring Boot, Java",
+      frontend: "React.js, JavaScript",
+      database: "Postgres SQL",
+    },
+    tools: ["JavaScript", "React.js", "Spring Boot", "Java", "PostgreSQL"],
+  },
+  {
+    id: 2,
+    title: "IT City Academy ",
+    imgSrc: "/itca.png",
+    imgAlt: "alibi esports landing page",
+    demoLink: "https://itcityacademy.uz/",
+    githubLink: "https://github.com/Alisher0903/alisher-itca",
+    description: `
+     Our goal is to increase the interest of young people in the regions in modern professions, to educate them as specialists in a certain field and thereby to increase their value.`,
+    technologies: {
+      backend: "Spring Boot, Java",
+      frontend: "Next.js, JavaScript",
+      database: "Postgres SQL",
+    },
+    tools: ["JavaScript", "Next.js", "Spring Boot", "Java", "PostgreSQL"],
+  },
+];
 
 const Projects = () => {
   useEffect(() => {
+    AOS.init({
+      duration: 1000, 
+      once: true, 
+    });
+
     const page = document.querySelector(".App");
     setTimeout(() => {
       page.classList.remove("animate_content");
     }, 2000);
-  });
+  }, []);
   return (
     <div className="projects-page-container">
       <div className="projects-landing">
-        <div className="project-headline">
-          <div className="project-header-img">
+        <div className="project-headline"  data-aos="zoom-in">
+          <div className="project-header-img" data-aos="zoom-in">
             <img src="/testimg.jpg" />
           </div>
           <div className="project-page-header">
@@ -26,115 +87,52 @@ const Projects = () => {
           </div>
         </div>
       </div>
-      <div id="project" className="projects-container">
-        <div className="project">
-          <div className="project-img">
-            <a
-              href="http://gsrlogistic.uz/"
-              target="_blank"
-              rel="noopen noreferrer"
-            >
-              <img
-                src="/peak-landing.png"
-                alt="peak performance landing page"
-              />
-            </a>
-          </div>
-          <div className="project-info">
-            <h2 className="project-title">DELIVERY SERVICE</h2>
-            <p>
-              The main purpose of this website is to deliver products from a
-              foreign country to its users on time! In this service, the main
-              goal of the website is to maintain the loyalty of its users, to
-              achieve this result, users can see their product vertual and see
-              when it will arrive and the price of the service, all the products
-              are added from the admin panel. Estimated earnings of this website
-              are between $1000-$10,000 depending on the sales of the products.
-            </p>
-            <p>
-              This program was completed and delivered to users directly within
-              10 days given 4 front end and 3 back end developers! The
-              technologies used to create this website 
-              <br/><br />
-              <b>Backend: Java</b><br />
-              <b>Front:React Js</b> <br />
-               <b>Database: Postgres SQL</b>
-            </p>
-            <div className="project-tools">
-              <p>Java</p>
-              <p>Tailwind CSS</p>
-              <p>React Js</p>
-              <p>PostgreSQL</p>
-            </div>
-            <div className="project-btns">
+      <div id="project" className="projects-container" >
+        {projects.map((project) => (
+          <div key={project.id} className="project">
+            <div className="project-img" >
               <a
-                href="http://gsrlogistic.uz/"
+                href={project.demoLink}
                 target="_blank"
-                rel="noopen noreferrer"
+                rel="noopener noreferrer"
               >
-                Demo
-              </a>
-              <a
-                href="https://github.com/Alisher0903/logistika"
-                target="_blank"
-                rel="noopen noreferrer"
-              >
-                GitHub
+                <img src={project.imgSrc} alt={project.imgAlt} />
               </a>
             </div>
-          </div>
-        </div>
-        <div className="project">
-          <div className="project-img">
-            <a
-              href="https://itcityacademy.uz/"
-              target="_blank"
-              rel="noopen noreferrer"
-            >
-              <img
-                src="/alibi-esports-landing.PNG"
-                alt="peak performance landing page"
-              />
-            </a>
-          </div>
-          <div className="project-info">
-            <h2 className="project-title">Alibi Esports</h2>
-            <p>
-              A website for the organization Alibi Esports, used to host
-              organization information, tournament registration, and
-              communication to players.
-            </p>
-            <p>
-              This was developed by incorporating UX/UI, form handling, RESTful
-              API, and database development to create an interactive and dynamic
-              environment that handled communication and tournament registration
-              while streamlining the user experience.
-            </p>
-            <div className="project-tools">
-              <p>JavaScript</p>
-              <p>React.js</p>
-              <p>Express.js</p>
-              <p>Node.js</p>
-              <p>PostgreSQL</p>
-            </div>
-            <div className="project-btns">
-              <a
-                href="https://pch14.uz/"
-                target="_blank"
-                rel="noopen noreferrer"
-              >
-                Demo
-              </a>
-              <a
-                href="https://github.com/SardorbekCoder07/Railway_z"
-                target="_blank"
-                rel="noopen noreferrer"
-              >
-                GitHub
-              </a>
+            <div className="project-info">
+              <h2 className="project-title">{project.title}</h2>
+              <p>{project.description}</p>
+              <p>
+                <b>Backend:</b> {project.technologies.backend}
+                <br />
+                <b>Frontend:</b> {project.technologies.frontend}
+                <br />
+                <b>Database:</b> {project.technologies.database}
+              </p>
+              <div className="project-tools">
+                {project.tools.map((tool) => (
+                  <p key={tool}>{tool}</p>
+                ))}
+              </div>
+              <div className="project-btns">
+                <a
+                  href={project.demoLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Demo
+                </a>
+                <a
+                  href={project.githubLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
